e1f68792dc2802facadca539475c46163df7e8fd
Kent Jordan <kentaki.dev@gmail.com>
2023-09-01 12:06:00 +0800

    Refactored valCreateUser

--- src/validators/users/index.val.ts
+++ src/validators/users/index.val.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 const valCreateUser = z.object({
     email: z.string().email(),
-    password: z.string(),
-    first_name: z.string(),
-    last_name: z.string(),
+    password: z.string().min(8),
+    first_name: z.string().min(2),
+    last_name: z.string().min(2),
 }).strict();
 
 export {
